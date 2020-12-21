b87b6e036a3649e63dd2d15cbbf7dca39ef01a64
Iliana <iliana.myh@gmail.com>
2020-12-21 12:46:07 +0200

    add private routes

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,35 +1,51 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 
 import Auth from './Auth/Auth';
 import Header from './Header/Header';
 import UsersList from './UsersList/UsersList';
 import PostsList from './PostsList/PostsList';
+import {useStoreState} from 'easy-peasy';
 
 export const routes = {
-	auth: '/auth',
-	registration: '/auth/registration',
-	login: '/auth/login',
-	passwordRecovery: '/auth/password-recovery',
-	passwordReset: '/auth/password-reset',
+  auth: '/auth',
+  registration: '/auth/registration',
+  login: '/auth/login',
+  passwordRecovery: '/auth/password-recovery',
+  passwordReset: '/auth/password-reset',
 
-	profile: '/profile/:id',
-	users: '/users',
-	posts: '/posts',
+  profile: '/profile/:id',
+  users: '/users',
+  posts: '/posts',
+};
+
+export const PrivateRoute = ({component: Component, privateWhenLoggedIn, ...props}) => {
+  const isLoggedIn = useStoreState(state => state.auth.isLoggedIn);
+
+  if (privateWhenLoggedIn && isLoggedIn) return <Redirect to={routes.profile} />;
+
+  return (
+      <Route {...props}
+             render={() => <>
+                 {isLoggedIn ? <Component/> : <Redirect to={routes.login}/>}
+               </>
+             }
+      />
+  );
 };
 
 const Routes = () => {
-	return (
-		<>
-			<Header />
-			<Switch>
-				<Route exact path='/' render={() => <Redirect to={routes.users} />} />
-				<Route path={routes.auth} component={Auth} />
-				<Route path={routes.users} component={UsersList} />
-				<Route path={routes.posts} component={PostsList} />
-			</Switch>
-		</>
-	);
+  return (
+      <>
+        <PrivateRoute path='/' component={Header} />
+        <Switch>
+          <Route exact path='/' render={() => <Redirect to={routes.users}/>}/>
+          <PrivateRoute privateWhenLoggedIn path={routes.auth} component={Auth}/>
+          <PrivateRoute path={routes.users} component={UsersList}/>
+          <PrivateRoute path={routes.posts} component={PostsList}/>
+        </Switch>
+      </>
+  );
 };
 
 export default Routes;
--- src/store/models/auth.js
+++ src/store/models/auth.js
@@ -1,3 +1,3 @@
 export const auth = {
-	isLoggedIn: false,
+	isLoggedIn: true,
 };

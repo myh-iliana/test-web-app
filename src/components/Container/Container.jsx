16cc1b7f01385b08f05f30e3ad65473b30e1d9ff
Iliana <iliana.myh@gmail.com>
2020-12-21 14:31:41 +0200

    add NotFound page

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -3,11 +3,12 @@ import { Container as ContainerUI } from 'semantic-ui-react';
 
 import styles from './Container.module.scss';
 
-const Container = ({ children, max, top = 0 }) => {
+const Container = ({ children, max, top = 0, ...props }) => {
 	return (
 		<ContainerUI
 			className={`${styles.container} ${styles.maxWidth}`}
 			style={{ '--max-width': `${max}px`, '--top': `${top}px` }}
+			{...props}
 		>
 			{children}
 		</ContainerUI>
--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,51 +1,53 @@
 import React from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Auth from './Auth/Auth';
 import Header from './Header/Header';
 import UsersList from './UsersList/UsersList';
 import PostsList from './PostsList/PostsList';
-import {useStoreState} from 'easy-peasy';
+import { useStoreState } from 'easy-peasy';
+import NotFound from './NotFound/NotFound';
 
 export const routes = {
-  auth: '/auth',
-  registration: '/auth/registration',
-  login: '/auth/login',
-  passwordRecovery: '/auth/password-recovery',
-  passwordReset: '/auth/password-reset',
+	auth: '/auth',
+	registration: '/auth/registration',
+	login: '/auth/login',
+	passwordRecovery: '/auth/password-recovery',
+	passwordReset: '/auth/password-reset',
 
-  profile: '/profile/:id',
-  users: '/users',
-  posts: '/posts',
+	profile: '/profile/:id',
+	users: '/users',
+	posts: '/posts',
 };
 
-export const PrivateRoute = ({component: Component, privateWhenLoggedIn, ...props}) => {
-  const isLoggedIn = useStoreState(state => state.auth.isLoggedIn);
+export const PrivateRoute = ({ component: Component, privateWhenLoggedIn, ...props }) => {
+	const isLoggedIn = useStoreState(state => state.auth.isLoggedIn);
 
-  if (privateWhenLoggedIn && isLoggedIn) return <Redirect to={routes.profile} />;
+	if (privateWhenLoggedIn && isLoggedIn) return <Redirect to={routes.profile} />;
 
-  return (
-      <Route {...props}
-             render={() => <>
-                 {isLoggedIn ? <Component/> : <Redirect to={routes.login}/>}
-               </>
-             }
-      />
-  );
+	return (
+		<Route {...props}
+					 render={() => <>
+						 {isLoggedIn ? <Component /> : <Redirect to={routes.login} />}
+					 </>
+					 }
+		/>
+	);
 };
 
 const Routes = () => {
-  return (
-      <>
-        <PrivateRoute path='/' component={Header} />
-        <Switch>
-          <Route exact path='/' render={() => <Redirect to={routes.users}/>}/>
-          <PrivateRoute privateWhenLoggedIn path={routes.auth} component={Auth}/>
-          <PrivateRoute path={routes.users} component={UsersList}/>
-          <PrivateRoute path={routes.posts} component={PostsList}/>
-        </Switch>
-      </>
-  );
+	return (
+		<>
+			<PrivateRoute path='/' component={Header} />
+			<Switch>
+				<Route exact path='/' render={() => <Redirect to={routes.users} />} />
+				<PrivateRoute privateWhenLoggedIn path={routes.auth} component={Auth} />
+				<PrivateRoute path={routes.users} component={UsersList} />
+				<PrivateRoute path={routes.posts} component={PostsList} />
+				<Route component={NotFound} />
+			</Switch>
+		</>
+	);
 };
 
 export default Routes;
